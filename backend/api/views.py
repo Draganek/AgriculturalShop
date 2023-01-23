673557939c3011ae8af26788825b434bc883cfdc
Draganek <radek.draganek@gmail.com>
2023-01-23 13:18:51 +0100

    part 1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,18 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Siemanko to jest testowa odpowiedź serwera"})
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data["params"] = dict(request.GET)
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+    return JsonResponse(data)
