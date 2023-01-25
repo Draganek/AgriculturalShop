21f6b906f2eacd6d607747611a9933f3c5e8d9cc
Draganek <Radek.Draganek@gmail.com>
2023-01-25 17:44:22 +0100

    v

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,17 @@
-import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-        data['id'] = model_data.id
-    return JsonResponse(data)
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
