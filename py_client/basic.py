36b840b96125b33349818e59e2ff5ffc4a5d891f
Draganek <Radek.Draganek@gmail.com>
2023-01-25 20:20:32 +0100

    serializers upd

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": 1})
+get_response = requests.post(endpoint, json={"product_id": 1})
 print(get_response.text)
 print(get_response.status_code)
 print(get_response.json())
