011371a4fb0c462561348adc6644f0ef4ebdcdf5
bartke117 <82660282+bartke117@users.noreply.github.com>
2023-01-25 20:18:36 +0100

    commit do widokow (#6)

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,13 +5,13 @@ from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        return Response(serializer.data)
+    return Response({"invalid":"not good data"}, status=400)
