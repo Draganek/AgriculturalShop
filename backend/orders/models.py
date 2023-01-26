a7190eb18e3da991a8bdb986021194dd79b430c0
grbwsky <123190387+grbwsky@users.noreply.github.com>
2023-01-26 16:49:37 +0100

    Niam (#11)
    * muj pierwszy komit
    
    * sqlite edt
    
    * users model edt
    
    * home url upd
    
    * orders model upd

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from products.models import Product
+from users.models import Client
 
 # Create your models here.
+class Order(models.Model):
+    order_number = models.CharField(max_length=120)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE,)
+    models.DecimalField(max_digits=15,decimal_places = 2, default=99.99)
+    date = models.DateField()
